4927bd458629bf0f1357c29a90b93a19992c4572
Heidy Kescilley <kescilley@gmail.com>
2025-11-09 21:28:25 -0300

    array

---
+++ src/TiposDeDados/Array.java
@@ -0,0 +1,21 @@
+package TiposDeDados;
+
+public class Array {
+    public static void main(String[] args) {
+
+        String[] ninja = new String[5];
+        ninja[0] = "Naruto Uzumaki";
+        ninja[1] = "Sasuke Uchiha";
+        ninja[2] = "Sakura Haruno";
+        ninja[3] = "Hinata Hyuga";
+        ninja[4] = "Kakashi Hatake";
+
+        for (int i = 0; i < ninja.length; i++) {
+            System.out.println("Ninja " + (i+1) + ": " + ninja[i]);
+        }
+
+        System.out.println(ninja);
+
+
+    }
+}
