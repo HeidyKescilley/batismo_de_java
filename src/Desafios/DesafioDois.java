a95f4a886ac49ba8eda9eeb5c24df80092ff2766
Heidy Kescilley <kescilley@gmail.com>
2025-11-10 21:39:46 -0300

    desafio array + scanner + outros conhecimentos

---
+++ src/Desafios/DesafioDois.java
@@ -0,0 +1,61 @@
+package Desafios;
+
+import java.util.Scanner;
+
+public class DesafioDois {
+
+    public static void main(String[] args) {
+
+        int option = 0;
+        String[] ninjas = new String[10];
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(ninjas[0]);
+
+        while (option != 3) {
+            System.out.println("\n===== Menu Ninja =====");
+            System.out.println("1. Cadastrar Ninja");
+            System.out.println("2. Listar Ninjas");
+            System.out.println("3. Sair");
+            System.out.print("Escolha uma opção: ");
+            option = input.nextInt();
+            input.nextLine();
+            switch (option) {
+                case 1:
+                    System.out.println("Qual nome do novo ninja?");
+                    for (int i = 0; i < ninjas.length; i++) {
+
+                        if (ninjas[i] == null) {
+                            ninjas[i] = input.nextLine();
+                            System.out.println(ninjas[i] + " adicionado a lista!");
+                            break;
+                        }
+
+                        if (i == ninjas.length - 1) {
+                            System.out.println("Acabou a vaga!");
+                        }
+                    }
+                    break;
+
+                case 2:
+                    int count = 1;
+                    System.out.println("===== Lista de Ninjas =====");
+                    for (String ninja : ninjas) {
+                        System.out.println("Ninja " + count + " - " + ninja);
+                        count++;
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Encerrando programa");
+                    break;
+
+                default:
+                    System.out.println("Opção invalida, vamos tentar de novo...");
+                    break;
+
+            }
+        }
+    }
+
+}
--- src/TiposDeDados/Array.java
+++ src/TiposDeDados/Array.java
@@ -3,18 +3,28 @@ package TiposDeDados;
 public class Array {
     public static void main(String[] args) {
 
-        String[] ninja = new String[5];
+        String[] ninja = new String[6];
         ninja[0] = "Naruto Uzumaki";
         ninja[1] = "Sasuke Uchiha";
         ninja[2] = "Sakura Haruno";
         ninja[3] = "Hinata Hyuga";
         ninja[4] = "Kakashi Hatake";
 
+        ninja = new String[7];
+
+        ninja[0] = "Hashirama Senju";
+        ninja[1] = "Tobirama Senju";
+        ninja[2] = "Tobirama Senju";
+        ninja[3] = "Minato Namikaze";
+        ninja[4] = "Tesunade Senju";
+        ninja[5] = "Kakashi Hatake";
+        ninja[6] = "Naruto Uzumaki";
+
+        System.out.println(ninja[7]);
+
         for (int i = 0; i < ninja.length; i++) {
             System.out.println("Ninja " + (i+1) + ": " + ninja[i]);
         }
-
-        System.out.println(ninja);
 
 
     }
